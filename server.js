40e6d50e3f64862fc246d55e9a641d88a487be8d
mcc <costanza+git@gmail.com>
2021-06-15 18:36:57 -0700

    add logout route, login & logout frontend scripts, session tracking

--- server.js
+++ server.js
@@ -3,6 +3,19 @@ const exphbs = require('express-handlebars');
 const path = require('path');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const session = require('express-session');
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: process.env.SESSION_SECRET,
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
 
 const PORT = process.env.PORT || 3001;
 
@@ -11,6 +24,8 @@ const hbs = exphbs.create({});
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+
+app.use(session(sess));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
